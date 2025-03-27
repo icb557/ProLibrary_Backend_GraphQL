ad18aeb6cff72ccb2d1ebe67561b900c8cc38a9d
cbisac <cortesbuitragoisac@gmail.com>
2025-03-26 22:46:56 -0500

    refactor login service

--- src/main/java/com/librarySpring/librarySpring/Entities/Login/services/LoginService.java
+++ src/main/java/com/librarySpring/librarySpring/Entities/Login/services/LoginService.java
@@ -5,7 +5,6 @@ import com.librarySpring.librarySpring.Entities.Login.model.CredentialsDTO;
 import com.librarySpring.librarySpring.Exceptions.LoginFailedException;
 import com.librarySpring.librarySpring.Interfaces.Command;
 import com.librarySpring.librarySpring.Security.JwtUtil;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,7 +13,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
-import javax.security.auth.login.LoginException;
 import java.util.HashMap;
 import java.util.Map;
 
